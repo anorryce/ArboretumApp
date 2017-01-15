10457182c258a82e848dce3c9cd70855fed6c5c7
anorryce <anorryce@hotmail.com>
2017-01-15 03:14:49 -0500

    Arboretum Project Commit. Added splash screen.

--- app/src/main/java/com/apress/gerber/arboretumapp/Splash.java
+++ app/src/main/java/com/apress/gerber/arboretumapp/Splash.java
@@ -1,7 +1,11 @@
 package com.apress.gerber.arboretumapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 /**
@@ -14,6 +18,29 @@ public class Splash extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
+        final ImageView iv = (ImageView) findViewById(R.id.loading);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation){
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation){
+                iv.startAnimation(an2);
+                finish();
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
         rLayout = (RelativeLayout) findViewById(R.id.relativelayout);
     }
 
